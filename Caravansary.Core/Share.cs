654c331f8dbcca5512d1871c8b885c85eea2ce42
Robert Jaskowski <robertjanjaskowski@gmail.com>
2021-08-31 17:54:57 +0200

    jhj

--- Caravansary.Core/Share.cs
+++ Caravansary.Core/Share.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
+//using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
